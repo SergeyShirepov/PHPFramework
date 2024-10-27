054643882f9aac23a1ae8f02890a0d3b27c1d0bc
SergeyShirepov <163455887+SergeyShirepov@users.noreply.github.com>
2024-10-27 23:53:02 +0300

    +

---
+++ app/widgets/Language/Language.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace app\widgets\;
+
+class Language
+{
+
+    protected $tpl;
+    protected $languages;
+    protected $language;
+
+    public  function __construct()
+    {
+        this->tpl = __DIR__ . 'lang_tpl.php';
+        $this->run();
+    }
+
+    protected function run()
+    {
+
+    }
+
+    public static function getLanguages ():array
+    {
+        return R::getAssoc("SELECT code,tittle, base, id FROM language ORDER BY base DESC");
+
+    }
+
+    public static function getLanguage ($language):array
+    {
+
+    }
+}
