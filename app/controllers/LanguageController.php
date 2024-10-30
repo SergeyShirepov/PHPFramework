6027dff2b5aa0d35ede79c19dd46765ce75fdeea
SergeyShirepov <163455887+SergeyShirepov@users.noreply.github.com>
2024-10-30 23:19:06 +0300

    Work with multilingualism is closed

---
+++ app/controllers/LanguageController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\controllers;
+
+class LanguageController extends AppController
+{
+    public function changeAction()
+    {
+        $lang = $_GET['lang'] ?? null;
+        var_dump($lang); die;
+
+    }
+
+}
